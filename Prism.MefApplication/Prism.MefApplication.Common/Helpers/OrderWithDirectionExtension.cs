51960a2346adff5081705bb45c6ccbf578134e6b
alexey-savin <alexeyv.savin@gmail.com>
2017-10-04 16:04:37 +0300

    Common assembly

---
+++ Prism.MefApplication/Prism.MefApplication.Common/Helpers/OrderWithDirectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prism.MefApplication.Common.Helpers
+{
+    public static class OrderWithDirectionExtension
+    {
+        public static IOrderedEnumerable<TSource> OrderByWithDirection<TSource, TKey>(
+            this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, bool descending = false)
+        {
+            return descending
+                ? source.OrderByDescending(keySelector)
+                : source.OrderBy(keySelector);
+        }
+    }
+}
