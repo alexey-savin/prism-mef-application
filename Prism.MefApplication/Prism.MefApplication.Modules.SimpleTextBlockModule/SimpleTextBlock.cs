134657b08efd475881c0ee94895ffa0a1a1eb3be
alexey-savin <alexeyv.savin@gmail.com>
2017-10-04 16:25:01 +0300

    Added Module with simple view

---
+++ Prism.MefApplication/Prism.MefApplication.Modules.SimpleTextBlockModule/SimpleTextBlock.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Mef.Modularity;
+using Prism.MefApplication.Modules.SimpleTextBlockModule.Views;
+using Prism.Modularity;
+using Prism.Regions;
+using System.ComponentModel.Composition;
+
+namespace Prism.MefApplication.Modules.SimpleTextBlockModule
+{
+    [ModuleExport(typeof(SimpleTextBlock))]
+    public class SimpleTextBlock : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        [ImportingConstructor]
+        public SimpleTextBlock(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(HelloPrismView));
+        }
+    }
+}
---
+++ Prism.MefApplication/Prism.MefApplication.Modules.SimpleTextBlockModule/Views/HelloPrismView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+
+namespace Prism.MefApplication.Modules.SimpleTextBlockModule.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для HelloPrismView.xaml
+    /// </summary>
+    [Export(typeof(HelloPrismView))]
+    public partial class HelloPrismView : UserControl
+    {
+        public HelloPrismView()
+        {
+            InitializeComponent();
+        }
+    }
+}
