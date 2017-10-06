d40bbd793fb8940438579974fbbc6d004ca86bb4
alexey-savin <alexeyv.savin@gmail.com>
2017-10-06 16:57:15 +0300

    Coverters added

---
+++ Prism.MefApplication/Prism.MefApplication.Common/Converters/ComposedConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Prism.MefApplication.Common.Converters
+{
+    [ContentProperty("Converters")]
+    public class ComposedConverter : IValueConverter
+    {
+        private readonly Collection<IValueConverter> _converters = new Collection<IValueConverter>();
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Collection<IValueConverter> Converters => _converters;
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            for (int i = 0; i < _converters.Count; i++)
+            {
+                value = _converters[i].Convert(value, targetType, parameter, culture);
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            for (int i = 0; i < _converters.Count; i++)
+            {
+                value = _converters[i].ConvertBack(value, targetType, parameter, culture);
+            }
+
+            return value;
+        }
+    }
+}
